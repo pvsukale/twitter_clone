6679b9a03b81d3d2c7333047adf21c1113fb0cba
pvsukale <pvsukale@gmail.com>
2018-01-10 17:19:12 +0530

    added test for valid and invalid logins

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,10 @@ before_save { email.downcase! }
 
 
 has_secure_password
+
+def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+end
+
 end
