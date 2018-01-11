ee903e923c9d3539dfbd80e527a1dacb66a838a3
pvsukale <pvsukale@gmail.com>
2018-01-11 17:26:06 +0530

    added password reset mailer and test

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,21 @@ class User < ApplicationRecord
     validates :name , presence: true, length: { maximum: 50}
     validates :email, presence: true, length: {maximum: 255} , format: {with: VALID_EMAIL_REGEX}, uniqueness: { case_sensitive: false }
     validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
-    attr_accessor :remember_token, :activation_token
-     before_save   :downcase_email
-     before_create :create_activation_digest
-    
+    attr_accessor :remember_token, :activation_token, :reset_token
+    before_save   :downcase_email
+    before_create :create_activation_digest
     has_secure_password
     
+    def create_reset_digest
+        self.reset_token = User.new_token
+        update_attribute(:reset_digest,  User.digest(reset_token))
+        update_attribute(:reset_sent_at, Time.zone.now)
+    end
+    
+      # Sends password reset email.
+    def send_password_reset_email
+        UserMailer.password_reset(self).deliver_now
+    end
     
     def remember
         self.remember_token = User.new_token
