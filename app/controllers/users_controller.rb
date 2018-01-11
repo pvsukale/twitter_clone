36ad98cb01d64c334cbdb0ab66e9ab9e3dbb6139
pvsukale <pvsukale@gmail.com>
2018-01-11 12:48:09 +0530

    added freindly fowards and correpsonding test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index,:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
   def new
     @user = User.new
   end
 
+  def index 
+  end
   def show
     @user = User.find(params[:id])
   end
@@ -44,6 +46,7 @@ class UsersController < ApplicationController
 
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,9 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get signup_path
-    assert_response :success
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
   end
 
 end
