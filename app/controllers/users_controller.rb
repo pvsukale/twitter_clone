3438aeb4deaadfeff0a7493c75b7860ce859f0e7
pvsukale <pvsukale@gmail.com>
2018-01-11 11:24:57 +0530

    added user update form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,10 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
@@ -19,6 +23,15 @@ class UsersController < ApplicationController
   end
 
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    
+    else
+      render 'edit'
+    end
+  end
+  
   private
 
     def user_params
