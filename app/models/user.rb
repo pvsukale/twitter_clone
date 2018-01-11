423d6903a886c4101d7a3355784063d6e3878c38
pvsukale <pvsukale@gmail.com>
2018-01-11 16:55:14 +0530

    more refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,14 +22,13 @@ class User < ApplicationRecord
     end
 
     def activate
-        update_attribute(:activated,    true)
-        update_attribute(:activated_at, Time.zone.now)
+         update_columns(activated: true, activated_at: Time.zone.now)
     end
 
     def send_activation_email
         UserMailer.account_activation(self).deliver_now
     end
-    
+
     def authenticated_digest?(token)
         digest = self.activation_digest
         return false if digest.nil?
