2dcdfeafb859877dbf02325e7247f640353890c4
pvsukale <pvsukale@gmail.com>
2018-01-11 11:36:06 +0530

    added test for valid craedentials in user update form

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -18,4 +18,18 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.field_with_errors'
   end
+
+  test "successful edit" do
+    get edit_user_path(@user)
+    assert_template 'users/edit'
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name, email: email, password: "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
