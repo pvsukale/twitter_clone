8b499204148b6c9f59100b20bf18a44a59e13cbb
pvsukale <pvsukale@gmail.com>
2018-01-12 13:37:30 +0530

    added validations and test to relationship model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,9 @@ class User < ApplicationRecord
     before_create :create_activation_digest
     has_secure_password
     has_many :microposts, dependent: :destroy
+    has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
 
     def feed
         microposts
