fbb2e47cacc469577c2aa2f4af3aceb63f22cd16
pvsukale <pvsukale@gmail.com>
2018-01-12 14:00:37 +0530

    modified test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -88,14 +88,18 @@ class UserTest < ActiveSupport::TestCase
 
 
   test "should follow and unfollow a user" do
-    michael = users(:michael)
-    archer  = users(:archer)
+    michael  = users(:michael)
+    archer   = users(:archer)
     assert_not michael.following?(archer)
     michael.follow(archer)
     assert michael.following?(archer)
+    assert archer.followers.include?(michael)
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+
+
 end
 
 
